d966838c3a724bea948d36a84dbd65ceee540280
Carlos Eduardo Dias Batista <carloseduardodiasbatista@gmail.com>
2024-11-16 20:13:36 -0300

    refactor: update initial route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
 import { TodoComponent } from './todo/todo.component';
 
-export const routes: Routes = [{ path: 'todo', component: TodoComponent }];
+export const routes: Routes = [{ path: '', component: TodoComponent }];
